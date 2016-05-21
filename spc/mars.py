0c293afea81d8ace6140a99c9b9181d0e47972e2
adamnew123456 <adamnew123456@gmail.com>
2016-05-21 14:55:28 -0400

    Start work on the MARS backend

---
+++ spc/mars.py
@@ -0,0 +1,62 @@
+"""
+MARS compiler backend - responsible for taking program events emitted by the
+driver and converting them into code.
+"""
+import logging
+
+from .errors import CompilerError
+from . import expressions
+from . import types
+
+LOGGER = logging.getLogger('spc.mars')
+
+class SymbolTable:
+    """
+    A lookup table which binds variables in a nested way, where access goes
+    from the current symbol table up to the parent.
+    """
+    def __init__(self, parent=None, is_global=False, is_builtin=False):
+        self.parent = parent
+        self.is_builtin = is_builtin
+        self.is_global = is_global
+        self.bindings = {}
+
+    def find(self, key):
+        """
+        Finds the symbol table in which the given key is bound, or returns
+        None if the symbol is not bound.
+        """
+        table = self
+        while table is not None:
+            if key in table.bindings:
+                return table
+            else:
+                table = table.parent
+
+        return None
+
+    def __getitem__(self, key):
+        table = self
+        while table is not None:
+            if key in table.bindings:
+                return table.bindings[key]
+            else:
+                table = table.parent
+
+        raise KeyError('Could not resolve symbol "{}"'.format(parent))
+
+    def __setitem__(self, key, value):
+        if key in self:
+            raise KeyError('Cannot overwrite existing definition of "{}"'.format(key))
+
+        self.bindings[key] = value
+
+    def __contains__(sef, key):
+        table = self
+        while table is not None:
+            if key in table.bindings:
+                return True
+            else:
+                table = table.parent
+
+        return False
