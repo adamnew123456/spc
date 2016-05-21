5379ee0539f3dea3c5d9bf98c1b90a719aa78197
adamnew123456 <adamnew123456@gmail.com>
2016-05-21 14:40:48 -0400

    Add an alignment calculator
    Useful for computing offsets into arrays and fields, and properly padding
    data on the stack.

--- spc/types.py
+++ spc/types.py
@@ -2,6 +2,7 @@
 All the different types that the compiler handles.
 """
 from collections import namedtuple
+from enum import Enum
 
 IntegerType = namedtuple('IntegerType', [])
 Integer = IntegerType()
@@ -44,3 +45,32 @@ def func_decl_to_ptr(func_decl):
     Converts a function declaration to a pointer.
     """
     return FunctionPointer(*func_decl)
+
+class Alignment(Enum):
+    Up = 1
+    Down = 2
+
+def align_address(addr, alignment, direction=Alignment.Up):
+    """
+    Returns the nearest address to the given address which is aligned to the 
+    given alignment.
+
+    >>> align_address(101, 4)
+    104
+    >>> align_address(101, 2)
+    102
+    >>> align_address(101, 4, direction=Alignment.Down)
+    100
+    >>> align_address(101, 2, direction=Alignment.Down)
+    100
+    """
+    if direction not in (Alignment.Up, Alignment.Down):
+        raise TypeError('Alignment direction must be Up or Down')
+
+    if addr % alignment == 0:
+        return addr
+
+    if direction == Alignment.Up:
+        return addr + alignment - (addr % alignment)
+    else:
+        return addr - (addr % alignment)
