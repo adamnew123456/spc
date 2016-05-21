4416e2d261e7f410a2075c6394798e69a37ee337
adamnew123456 <adamnew123456@gmail.com>
2016-05-20 22:02:24 -0400

    Add some utility functions to the types module

--- spc/types.py
+++ spc/types.py
@@ -23,3 +23,22 @@ Struct = namedtuple('Struct', ['fields'])
 FunctionDecl = namedtuple('FunctionDecl', ['return_type', 'params'])
 
 AliasDef = namedtuple('AliasDef', ['type'])
+
+# Raw types are types which can be used as variables
+RAW_TYPES = (types.IntegerType, types.ByteType, types.TypeName, 
+        types.PointerTo, types.ArrayOf, types.FunctionPointer)
+
+def decay_if_array(type_obj):
+    """
+    Decays arrays types into pointers.
+    """
+    if isinstance(type_obj, types.ArrayOf):
+        return type_obj.PointerTo(type_obj.type)
+    else:
+        return type_obj
+
+def func_decl_to_ptr(func_decl):
+    """
+    Converts a function declaration to a pointer.
+    """
+    return FunctionPointer(*func_decl)
