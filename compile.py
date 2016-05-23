ed7658c289e5b75e499e13e4838b2464da99c826
adamnew123456 <adamnew123456@gmail.com>
2016-05-22 21:51:19 -0400

    Fix some issues in the compiler script

--- compile.py
+++ compile.py
@@ -8,23 +8,26 @@ BACKEND = None
 IN_FILE = sys.stdin
 OUT_FILE = sys.stdout
 
-FLAGS, POS_ARGS = getopt.getopt(sys.argv, 'b:o:')
+FLAGS, POS_ARGS = getopt.getopt(sys.argv[1:], 'b:o:')
 for opt, arg in FLAGS:
     if opt == '-b':
         try:
-            BACKEND = importlib.import_module(arg, 'spc')
-        except ImportError:
+            BACKEND = importlib.import_module('spc.' + arg)
+        except ImportError as err:
             print('Invalid backend:', arg, file=sys.stderr)
+            print('::', err, file=sys.stderr)
             sys.exit(1)
     elif opt == '-o':
         try:
             OUT_FILE = open(arg, 'w')
-        except IOError:
+        except IOError as err:
             print('Cannot open output file:', arg, file=sys.stderr)
+            print('::', err, file=sys.stderr)
             sys.exit(1)
 
 if BACKEND is None:
-    print('-b argument required', file=sys.stderr)
+    print('-b BACKEND argument required', file=sys.stderr)
+    sys.exit(1)
 
 if POS_ARGS:
     try:
@@ -34,13 +37,13 @@ if POS_ARGS:
         sys.exit(1)
 
 with IN_FILE, OUT_FILE:
-    lex = lexer.Lexer(FILE)
+    lex = lexer.Lexer(IN_FILE)
     lexed_lists = lexer.to_list(lex.lex())
 
     backend = BACKEND.get_backend(OUT_FILE)
     drv = driver.Driver(lexed_lists, backend)
     try:
         drv.compile()
-    except CompilerError as err:
-        print(err, file=sys.stderr)
+    except errors.CompilerError as err:
+        print(err.message, file=sys.stderr)
         sys.exit(1)
