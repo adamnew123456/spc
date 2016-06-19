6bbdbf8336f29b2496ba570020c69ebdb9bafdd1
adamnew123456 <adamnew123456@gmail.com>
2016-06-19 10:44:36 -0400

    Wrote some basic system calls for the Linux backend

---
+++ arch/linux_x86.lisp
@@ -0,0 +1,28 @@
+(declare
+ (linux.exit (function byte integer))
+ (linux.read (function integer integer string integer))
+ (linux.write (function integer integer string integer)))
+
+(export linux.exit linux.read linux.write)
+
+(assemble linux.exit
+ "movl $1, %eax
+  movl 4(%esp), %ebx
+  int $0x80
+  ret")
+
+(assemble linux.read
+ "movl $3, %eax
+  movl 4(%esp), %ebx
+  movl 8(%esp), %ecx
+  movl 12(%esp), %edx
+  int $0x80
+  ret")
+
+(assemble linux.write
+ "movl $4, %eax
+  movl 4(%esp), %ebx
+  movl 8(%esp), %ecx
+  movl 12(%esp), %edx
+  int $0x80
+  ret")
