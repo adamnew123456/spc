21c390af7d79d8fa375c54e6ea3804dfc2d96f5c
Adam Marchetti <adamnew123456@gmail.com>
2016-07-06 21:15:11 -0400

    Test require changes with var-def?

---
+++ samples/mars/cached-require.lisp
@@ -0,0 +1,16 @@
+(require "lib/assert.lisp")
+(require "lib/io.lisp") ;; Assert requires IO
+(declare
+ (has-io.print (function integer))
+ (main (function byte)))
+
+(define has-io.print ()
+ (declare)
+ (*if (var-def? "io.print")
+  (return 1)
+  (return 0)))
+
+(define main ()
+ (declare
+  (msg (ascii "We should have loaded io.print")))
+ (assert (== (has-io.print) 1) msg))
