0805edb88ef39989ae10404b331b4fd82644c119
adamnew123456 <adamnew123456@gmail.com>
2016-08-21 20:52:34 -0400

    Add the basic samples for byte syntax

---
+++ samples/linux_x86/byte.lisp
@@ -0,0 +1,18 @@
+(require "lib/assert.lisp")
+(declare
+ (main (function byte)))
+
+(define main ()
+ (declare
+  (x byte)
+  (y (ascii "q\n"))
+  
+  (msg.1 (ascii "Byte literal 'q' should be equal to y[0]"))
+  (msg.2 (ascii "Byte literal '\n' should be equal to y[1]")))
+
+ (block
+  (set x #q)
+  (assert (== x (array y 0)) msg.1)
+
+  (set x #\n)
+  (assert (== x (array y 1)) msg.2)))
